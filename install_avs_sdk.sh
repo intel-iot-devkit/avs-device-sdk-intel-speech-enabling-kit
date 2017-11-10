2e358047b87f1515c7d7deca88cbd0dc73385ab2
Midkiff, Kevin <kevin.midkiff@intel.com>
2017-11-10 09:21:17 -0800

    Add mimic git repo locations

--- install_avs_sdk.sh
+++ install_avs_sdk.sh
@@ -2,8 +2,8 @@
 
 HOME="/home/${SUDO_USER}"
 
-GIT_REPO_URL="/test_repo.git"
-GIT_DRIVER_URL=""
+GIT_REPO_URL="$HOME/sdk-git-mimic.git"
+GIT_DRIVER_URL="$HOME/driver-git-mimic.git"
 PORT_AUDIO_URL="http://www.portaudio.com/archives/pa_stable_v190600_20161030.tgz"
 PORT_AUDIO_TAR="pa_stable_v190600_20161030.tgz"
 CONFIG_JSON="AlexaClientSDKConfig.json"
