44614ba4fd46bdc22706e007a2634285d4d7fa74
Anthony Loeppert <anthony.loeppert@intel.com>
2018-03-02 10:42:45 -0800

    Enable socketio c++ client.
    The socket.io c++ client was recommended by the
    (future) display server software stack.
    
    This change enables the compilation and library
    access from the AVS SDK.
    
    Signed-off-by: Anthony Loeppert <anthony.loeppert@intel.com>

--- Ubuntu/install_avs_sdk.sh
+++ Ubuntu/install_avs_sdk.sh
@@ -14,6 +14,11 @@ PORT_AUDIO_URL="http://www.portaudio.com/archives/$PORT_AUDIO_TAR"
 CURL_DIR="curl-7.57.0"
 CURL_TAR="$CURL_DIR.tar.gz"
 CURL_URL="https://curl.haxx.se/download/$CURL_TAR"
+BOOST_DIR="boost_1_66_0"
+BOOST_VER="1.66.0"
+BOOST_TAR="$BOOST_DIR.tar.gz"
+BOOST_URL="https://dl.bintray.com/boostorg/release/$BOOST_VER/source/$BOOST_TAR"
+BOOST_ROOT="/usr/local"
 
 top="$(pwd)"
 avs_top="$top/avs"
@@ -31,6 +36,24 @@ if [[ ! -d $third_party_dir/$CURL_DIR ]]; then
 cd $third_party_dir && wget $CURL_URL && tar xvf $CURL_TAR && cd $CURL_DIR && ./configure --with-nghttp2 --prefix=/usr/local && make -j4 && sudo make install
 fi
 
+if [ ! -d $third_party_dir/$BOOST_DIR ]; then
+    cd $third_party_dir
+    wget $BOOST_URL
+    tar -xvf $BOOST_TAR
+    cd $BOOST_DIR
+    # strap in, this takes a long time
+    ./bootstrap.sh && sudo ./b2 install
+fi
+
+if [ ! -d $third_party_dir/socket.io-client-cpp ]; then
+    cd $third_party_dir
+    git clone --recurse-submodules https://github.com/socketio/socket.io-client-cpp.git
+    cd socket.io-client-cpp/lib/websocketpp && git apply $avs_top/avs-device-sdk-intel-speech-enabling-kit/Ubuntu/websocketpp.patch
+    cd $third_party_dir/socket.io-client-cpp
+    cmake -DBOOST_ROOT:STRING=$BOOST_ROOT -DBOOST_VER:STRING=$BOOST_VER ./
+    make -j4 && sudo make install
+fi
+
 cd $avs_top
 
 pushd $avs_top/avs-device-sdk-intel-speech-enabling-kit
@@ -38,8 +61,8 @@ if [[ ! -d application-necessities ]]; then
 tar xvf application-necessities.tar.gz -C $avs_top
 fi
 cp Ubuntu/automated_install.sh $avs_top
-cat Ubuntu/startavs|sed "s#\\[AVSROOT\\]#$top"#>$top/startavs
-chmod a+x $top/startavs
+cat Ubuntu/startavs|sed "s#\\[AVSROOT\\]#$avs_top"#>$avs_top/startavs
+chmod a+x $avs_top/startavs
 popd
 
 if [[ ! -d sdk-build ]]; then
@@ -55,7 +78,10 @@ cd sdk-build && cmake ../avs-device-sdk-intel-speech-enabling-kit \
     -DPORTAUDIO=ON \
     -DPORTAUDIO_LIB_PATH=$third_party_dir/portaudio/lib/.libs/libportaudio.a \
     -DPORTAUDIO_INCLUDE_DIR=$third_party_dir/portaudio/include \
-    -DEMIT_RAW_JSON_PAYLOAD=ON
+    -DSOCKETIO_INCLUDE_DIR=$third_party_dir/socket.io-client-cpp/build/include \
+    -DSOCKETIO_LIB_PATH=$third_party_dir/socket.io-client-cpp/build/lib/Release/libsioclient.a \
+    -DBOOST_LIB_PATH=$BOOST_ROOT/lib/libboost_system.a
+
 
 make -j4
 
@@ -67,5 +93,4 @@ else
   exit
 fi
 
-cd $avs_top && ./automated_install.sh
-
+echo "Please reboot system if running this script for the first time."
