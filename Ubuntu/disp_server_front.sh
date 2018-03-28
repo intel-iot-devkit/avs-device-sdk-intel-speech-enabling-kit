b7aca2b616ce14eeddd997f8b3dbe4d858a47db5
Anthony Loeppert <anthony.loeppert@intel.com>
2018-03-28 14:08:49 -0700

    Make certain "ng" is installed in display server kickoff script.
    Signed-off-by: Anthony Loeppert <anthony.loeppert@intel.com>

--- Ubuntu/disp_server_front.sh
+++ Ubuntu/disp_server_front.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt install ng-common
+
 firefox -new-tab http://localhost:3001 &
 #run from the avs top
 cd third-party/iot-server/ui-generator && ng build --outputPath ../public --watch
